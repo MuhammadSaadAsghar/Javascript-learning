5683366d80c2dd48b12c8e8b4f44310c331ef2aa
Muhammad Saad Asghar <muhammadsaadasghar408@gmail.com>
2026-03-05 08:56:49 +0500

    permitive data type

---
+++ Data type & type system/script.js
@@ -0,0 +1,42 @@
+// Data types matlab data ka type 
+
+// Permitive -> asi sari values jin ko copy krna pr tumy ak real copy mil jaya
+// String number boolean BigInt symbol null undefind
+
+// Refrence -> inko copy krna pr apko real copy ni mily gi but apko Refrence mila ga perent ka
+// array function object
+
+
+// Example:-
+let name = "Saad";
+let city = 'Faisalabad';
+
+// (2) Number:-
+// Integer, decimal dono number hi hotay hain.
+let age = 20;
+let price = 99.99;
+
+// ✅ (3) Boolean
+// True / false values.
+let isLoggedIn = true;
+
+// ✅ (4) Undefined
+// Jis variable ko value assign na ki ho.
+// Ap ne variable banya lakin us ko value ni di to jo value by default set hogi wo undefined hogi
+let a;
+console.log(a); // undefined
+
+// ✅ (5) Null
+// Value khud se empty set krna take bad me us koi value de di jaya
+// Matlab jan boojh kr koi value ni di
+let b = null;
+
+// ✅ (6) BigInt
+// Bohat bade numbers (64-bit se zyada).
+let big = 12345678901234567890n;
+
+// ✅ (7) Symbol
+// Unique & immutable values — mostly advanced cases me use hota hai.
+let s = Symbol("id");
+
+

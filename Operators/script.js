486004c174398411d1cb292a94fd5d0153bd5e6d
Muhammad Saad Asghar <muhammadsaadasghar408@gmail.com>
2026-03-12 15:10:02 +0500

    operators

---
+++ Operators/script.js
@@ -0,0 +1,61 @@
+// operators 
+
+// Arthmatics  comparison Assignment Logical  unary
+
+// Arthmatics operators
+// + 
+let a =10;
+let b =20;
+ console.log(a+b);
+ 
+ // - 
+ let num =30;
+ let num2 =20;
+  console.log(num-num2);
+
+// *
+let a1 = 2;
+let b2 = 3;
+console.log(a1*b2);
+//  /
+let a3 = 12;
+let b3 = 3;
+console.log(a3/b3);
+// %
+let a4 = 12;
+let b4 = 3;
+console.log(a4%b4);
+// **
+let a5 = 2;
+let b5 = 3;
+console.log(a5**b5);
+
+// comparison operators
+// Comparison operators are used to compare two values in JavaScript and return a Boolean result (true or false).
+
+
+// 1️ Equal To ==
+// Checks if two values are equal (does type conversion).
+5 == "5"     // true
+// 2️ Strict Equal ===
+// Checks value and data type.
+5 === "5"   // false
+
+// 3️ Not Equal !=
+// Checks if values are not equal.
+5 != 10   // true
+// 4️ Strict Not Equal !==
+// Checks value and type are not equal.
+5 !== "5"   // true
+
+// 5️ Greater Than >
+10 > 5   // true
+// 6️ Less Than <
+3 < 8   // true
+
+// 7️ Greater Than or Equal >=
+10 >= 10   // true
+
+// 8️ Less Than or Equal <=
+5 <= 7   // true
+

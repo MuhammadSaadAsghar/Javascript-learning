9817818bdbe7d8abd91b2c81c82e80591f9adad6
Muhammad Saad Asghar <muhammadsaadasghar408@gmail.com>
2026-03-18 11:05:12 +0500

    loops

---
+++ Loops/question/script.js
@@ -0,0 +1,15 @@
+// print number from 1 to 10 using for loop
+
+// for (let i = 0; i <=10; i++) {
+    //   console.log(i)
+    
+    // }
+
+
+    // print number from 10 to 1 using while loop
+
+    let i =10;
+    while (i>1) {
+        console.log(i) 
+    }
+    i--;
---
+++ Loops/script.js
@@ -0,0 +1,42 @@
+// Loops -> bar bar repeat hona
+
+// for
+// jis me humy pata ho ka loop kaha se start ho kr kaha tk chla ga
+
+// for (let i = 0; i <= 30; i++) {
+//     console.log(i);   
+// }
+
+// while
+// jis me humy pata ho ka kaha se start hoga but humy ye pata ha rukna kb ha lakin ye ni pata ha kaha ruka ga
+
+// let i=1;
+// while (i<=40) {
+//     console.log(i);
+//   i++;  
+// }
+
+// do while
+// is me condition false bi ho phir bi ak bar loop chla ga
+// let i=12;
+// do {
+//     console.log(i)
+//     i++;
+// } while (i<=15);
+
+// break in Loops
+//  for (let i = 0; i <= 30; i++) {
+//     console.log(i);   
+//     if (i===20) {
+//         break;
+        
+//     }
+// }
+
+// continue in loop
+//  for (let i = 0; i <= 30; i++) {
+//         if (i===20) {
+//                 continue;
+//             }        
+//             console.log(i);   
+// }

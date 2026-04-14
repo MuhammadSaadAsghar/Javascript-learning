0c3edb9fb052315c475553057b134c74c6e6c250
Muhammad Saad Asghar <muhammadsaadasghar408@gmail.com>
2026-04-14 23:23:01 +0500

    array question

---
+++ arrays/arrays question/script.js
@@ -0,0 +1,12 @@
+// create an array with 3 fruits and print second fruits
+
+let fruits =["apple","banana","orange"];
+
+console.log(fruits[1]);
+
+// add "mango" at the end and "pineapple" at the beginning  of this array 
+let fruits1 =["apple","banana","orange"];
+
+fruits1.push("mango")
+fruits1.unshift("pineapple")
+console.log(fruits1)

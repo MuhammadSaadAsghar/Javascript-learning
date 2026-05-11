69d41ea7a334e1fe07cad396c50457ed04ea82e1
Muhammad Saad Asghar <muhammadsaadasghar408@gmail.com>
2026-05-11 11:39:06 +0500

    form validation complete

--- form validation/script.js
+++ form validation/script.js
@@ -4,6 +4,8 @@ let form=document.querySelector("form");
 
 form.addEventListener("submit",function(dets){
 dets.preventDefault()
+ document.querySelector("#emailerror").textContent=""
+ document.querySelector("#passworderror").textContent=""
 
 const emailRegix=/^[^\s@]+@[^\s@]+\.[^\s@]+$/
 const passwordRegix=/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
@@ -11,10 +13,11 @@ const passwordRegix=/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
 let emailans=emailRegix.test(email.value)
 let passwordans=passwordRegix.test(password.value)
 
+ let isvalid=true;
 if (!emailans) {
     document.querySelector("#emailerror").textContent="email is incorrect"
     document.querySelector("#emailerror").style.display="initial"
-    
+    isvalid=false;
 }else{
     document.querySelector("#emailerror").style.display="none"
     
@@ -24,10 +27,15 @@ if (!emailans) {
 if (!passwordans) {
     document.querySelector("#passworderror").textContent="password is incorrect"
      document.querySelector("#passworderror").style.display="initial"
+     isvalid=false;
     }
     else{
     document.querySelector("#passworderror").style.display="none"
 
+}
+
+if (isvalid) {
+    document.querySelector("#resultmessage").textContent="login is successfull"
 }
 
 

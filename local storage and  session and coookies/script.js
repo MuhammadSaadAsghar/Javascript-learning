17f0a2233de9bc6dca64012ad5b6d339373b5607
Muhammad Saad Asghar <muhammadsaadasghar408@gmail.com>
2026-05-12 23:35:10 +0500

    local storage

---
+++ local storage and  session and coookies/script.js
@@ -0,0 +1,17 @@
+// local storage
+
+// store kasa kra 
+localStorage.setItem("name","saad")
+
+
+// data fatch kasa kra 
+let val=localStorage.getItem("name")
+
+
+// remove kasa kra
+localStorage.removeItem("name")
+
+
+// update kasa kra
+localStorage.setItem("name","saadasghar")
+// setitem agr phly se store na ho to value store krta ha agr phly se store ha to us ko update krta ha
